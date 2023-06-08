65f7d94d40361e605a75095ed53680cf2fdae99a
jakubCF <77362128+jakubCF@users.noreply.github.com>
2023-06-08 22:09:59 +0200

    icons and changes to content.js

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 const FAVICON_URL = "https://www.notion.so/images/favicon.ico";
 
 const getFaviconDomEl = () => document.querySelector("link[rel~='icon']");
-var getTitle = document.querySelector('title');
+const getTitle = () => document.querySelector('title');
 
 const setFavicon = () => {
   const linkEl = getFaviconDomEl();
@@ -18,7 +18,7 @@ const observer = new MutationObserver((mutationList) => {
 
 const observerTitle = new MutationObserver((mutationList) => {
   mutationList.forEach((mutation) => {
-    if (mutation.target.startsWith("Notion")) {
+    if (!mutation.target.textContent.startsWith("Notion")) {
       document.title = "Notion - " + document.title;
     }
   })
@@ -27,6 +27,7 @@ const observerTitle = new MutationObserver((mutationList) => {
 const options = {
   attributeFilter: ['href'],
 };
+var configObserve = {childList: true};
 
 observer.observe(getFaviconDomEl(), options);
-observerTitle.observe(getTitle());
+observerTitle.observe(getTitle(), configObserve);
